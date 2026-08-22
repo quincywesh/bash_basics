eef1469cdae00c8553212f8ea081daa5c4194424
quincywesh <quincywesh@gmail.com>
2026-08-22 10:15:50 -0400

    Update 11-song.sh operating system theme

--- 11-song.sh
+++ 11-song.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
+while [ true ]; do
+
+    # Ask the user to choose an operating system
+    read -p "Choose an operating system: windows, mac, linux, or q to end > " system
+
+    case $system in
+        windows|Windows)
+            echo "Windows is widely used on personal computers."
+            ;;
+        mac|Mac)
+            echo "macOS is Apple's operating system for Mac computers."
+            ;;
+        linux|Linux)
+            echo "Linux is a popular open-source operating system."
+            ;;
+        q|Q)
+            echo "Thanks for choosing an operating system!"
+            exit 0
+            ;;
+        *)
+            echo "That is not one of the listed operating systems."
+            ;;
+    esac
+
+done
+
+# exercise completed: Changed the input question and case
+# statement from musical notes to operating systems.#!/bin/sh
 
 
 while [ true ]; do
